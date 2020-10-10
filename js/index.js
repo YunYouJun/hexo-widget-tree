d6709ff2d609ef1d0f82e89d115168ded691e1a3
YunYouJun <me@yunyoujun.cn>
2020-10-10 13:37:49 +0800

    :sparkles: auto active & expand post in folder by pathname

--- js/index.js
+++ js/index.js
@@ -31,6 +31,49 @@ function initWidgetTree() {
     icon.classList.toggle("gg-chevron-right");
     icon.classList.toggle("gg-chevron-left");
   });
+
+  // modify toc link
+  const tocItems = document.querySelectorAll(".tree-post-toc-item");
+  if (tocItems) {
+    tocItems.forEach((tocItem) => {
+      const link = tocItem.parentNode.parentNode.querySelector("a").href;
+      const tocLink = tocItem.querySelector("a");
+      tocLink.href = link + new URL(tocLink.href).hash;
+    });
+  }
+
+  const postLinks = document.querySelectorAll(".tree-list-post-link");
+  postLinks.forEach((postLink) => {
+    const pathname = new URL(postLink.href).pathname;
+    if (pathname === document.location.pathname) {
+      postLink.style.color = "steelblue";
+      activePostLink(postLink.parentNode);
+    }
+  });
+
+  function activePostLink(node) {
+    if (!node.classList.contains("open")) {
+      node.classList.add("open");
+      openFolder(node);
+    }
+    if (node.parentNode.parentNode.classList.contains("tree-list-item")) {
+      activePostLink(node.parentNode.parentNode);
+    }
+  }
+
+  function openFolder(node) {
+    const icon = node.querySelector("i");
+    if (icon) {
+      if (icon.classList.contains("gg-file-add")) {
+        icon.classList.toggle("gg-file-add");
+        icon.classList.toggle("gg-file-remove");
+      }
+      if (icon.classList.contains("gg-folder-add")) {
+        icon.classList.toggle("gg-folder-add");
+        icon.classList.toggle("gg-folder-remove");
+      }
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", initWidgetTree);
